2e2e7387a3b31b54374bb13609e8a5d4197ae618
HaroldDeng <dengz5@rpi.edu>
2020-04-15 20:22:25 +0800

    update SRT

--- SRT.py
+++ SRT.py
@@ -1,9 +1,9 @@
 """
-Shortest remaining time, when a process arrives, before it enters the ready 
+Shortest remaining time, when a process arrives, before it enters the ready
     queue, if it has a CPU burst time that is less than the remaining time of
-    the currently running process, a preemption occurs. When such a preemption 
-    occurs, the currently running process is added back to the ready queue. 
-    
+    the currently running process, a preemption occurs. When such a preemption
+    occurs, the currently running process is added back to the ready queue.
+
 Author
     Zhihao Deng (dengz5@rpi.edu)
 
@@ -14,20 +14,73 @@ from util import Action, Process
 
 
 class SRT:
-    def __init__(self, processes: [Process]):
+    def __init__(self, procs: [Process], tau0: int):
         self.endedQueue = []          # processes in terminated state
         self.readyQueue = []          # processes in ready state
         self.clock = 0                # in ms
+        self.tau = tau0                # in ms
 
         # processes in state other than ready and terminated
-        self.actionQueue = copy.deepcopy(processes)
+        self.actionQueue = copy.deepcopy(procs)
 
     def simulate(self):
-        pass
+        # print all processes
+        for p in self.actionQueue:
+            print(
+                "Process {:s} [NEW] (arrival time {:d} ms) {:d} CPU bursts".format(
+                    p.name, p.arrival_time, len(p.burst_time)
+                ))
+        print("time 0ms: Simulator started for SRT [Q <empty>]")
+        self._sort()
 
-    def resume(self, pros: Process) -> [Process]:
+        retVal = self.resume()
+        while retVal[0] != None:
+            print("time {:d}ms: Process {:s} (tau {d}ms)".format(
+                self.clock, retVal[0].name, self.tau), end="")
+            if retVal[0].action == Action.new:
+                # new process arrived
+                print("arrived added to ready queue ", end="")
+
+            elif retVal[0].action == Action.ready:
+                # process ready for CPU burst
+                print("started using the CPU for {:d}ms burst".format(
+                    retVal[0].burst_time[retVal[0].burst_index]), end="")
+
+            elif retVal[0].action == Action.running:
+                if retVal[1] != None:
+                    # CPU bursting process being preempted
+                    pass
+                else:
+                    pass
+            retVal = self.resume(retVal[0])
+
+            # print readyQueue
+            print("[Q")
+            if len(self.readyQueue) == 0:
+                print(" <empty>")
+            else:
+                for p in self.readyQueue:
+                    print(" " + p.name, end="")
+            print("]")
+
+    """
+        Core algorithm of SRT implementation. On start, pros should be None, 
+            after start, pros should be the first element of last returned 
+            value.
+
+        @param pros input process.
+        @return a Process list with list length is 2
+            [None, None] if actionQueue is empty
+            [P1, P2] if Process P1 is preempted by Process P2
+            [P1, None] otherwise
+    """
+
+    def resume(self, pros: Process = None) -> [Process]:
         retVal = [None, None]
-        if pros.action == Action.new:
+
+        if pros == None:
+            pass
+        elif pros.action == Action.new:
             pros.action = Action.ready
             self.readyQueue.append(pros)
         elif pros.action == Action.ready:
@@ -51,16 +104,18 @@ class SRT:
             # if remain burst time is greater than I/O ended
             #     process burst time, premmption
             if btProc.action_exit - self.clock > pros.burst_time[pros.burst_index]:
-                # remove bursting process from CPU
+                # remove CPU bursting process from CPU
+                self.actionQueue.remove(btProc)
+                self.readyQueue.append(btProc)
+                # updates
                 btProc.preempt_count += 1
                 btProc.burst_time[btProc.burst_index] = btProc.action_exit - self.clock
                 btProc.action = Action.ready
-                self.actionQueue.remove(btProc)
-                self.readyQueue.append(btProc)
 
-                # replacement start CPU burst
+                # assign new process to CPU
                 pros.action = Action.running
-                pros.action_exit = self.clock + pros.burst_time[pros.burst_index]
+                pros.action_exit = self.clock + \
+                    pros.burst_time[pros.burst_index]
                 self.actionQueue.append(pros)
 
                 retVal[1] = btProc
@@ -79,6 +134,7 @@ class SRT:
             retVal[0] = self.actionQueue.pop(0)
         return retVal
 
+    # sort actionQueue by action_exit
     def _sort(self):
 
         # (a) CPU burst completion
--- project1.py
+++ project1.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     bound = 0       # upper bound for valid pseudo-random numbers
     procs = 0       # process count
     t_cs = 0        # context switch time in ms
-    alpha = 0.0     # alpah for exponential averaging
+    alpha = 0.0     # alpha for exponential averaging
     t_slice = 0     # time slice in ms
     add_end = True  # define whether processes are added to the end or the beginning of the ready queue
 
@@ -56,29 +56,28 @@ if __name__ == "__main__":
     lcg.srand48(seed)
 
     # create process
-    # !!! procsList is shared amount all operation, DO NOT modify the list
-    procsList = []
+    # !!! procs is shared amount all operation, DO NOT modify
+    procs = []
     for shift in range(procs):
         p = Process(chr(65 + shift))
 
         p.arrival_time = math.floor(-math.log(lcg.drand48()) / lambda_)
         while (p.arrival_time > bound):
             p.arrival_time = math.floor(-math.log(lcg.drand48()) / lambda_)
-        
-        p.arrival_time *= 1000 # in ms
+
         p.action_exit = p.arrival_time
         p.action = Action.new
 
-        procsList.append(p)
+        procs.append(p)
 
     # assign CPU burst and I/O block count
-    for p in procsList:
-        p.burst_time = [0] * int(lcg.drand48() * 100) + 1 # [1, 100]
+    for p in procs:
+        p.burst_time = [0] * (int(lcg.drand48() * 100) + 1)  # [1, 100]
         p.block_time = [0] * (len(p.burst_time) - 1)
 
     # assign CPU burst and I/O block time
-    for p in procsList:
-        for i in range(p.block_time):
+    for p in procs:
+        for i in range(len(p.block_time)):
             # CPU burst
             p.burst_time[i] = math.ceil(-math.log(lcg.drand48()) / lambda_)
             while (p.burst_time[i] > bound):
@@ -88,14 +87,12 @@ if __name__ == "__main__":
             p.block_time[i] = math.ceil(-math.log(lcg.drand48()) / lambda_)
             while (p.block_time[i] > bound):
                 p.block_time[i] = math.ceil(-math.log(lcg.drand48()) / lambda_)
-            
-            p.burst_time[i] *= 1000 # in ms
-            p.block_time[i] *= 1000
-        
+
         # last CPU burst
         p.burst_time[-1] = math.ceil(-math.log(lcg.drand48()) / lambda_)
         while (p.burst_time[-1] > bound):
             p.burst_time[-1] = math.ceil(-math.log(lcg.drand48()) / lambda_)
-        p.burst_time[-1] *= 1000
 
     # start simulation
+    srt = SRT(procs, math.ceil(1/lambda_))
+    srt.simulate()
